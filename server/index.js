4a72142270d06979e46321ab56f7d84367e4086e
rodk <rod2010rod2010@hotmail.com>
2016-10-01 17:26:37 -0400

    completed #2

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev.js';
+
 var port = 8000;
 let app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
   // res.send('Hello Ridgewood!')
