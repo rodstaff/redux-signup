98a5465ebe5fb8ba4f23c03982cd4527172f9b35
rodk <rod2010rod2010@hotmail.com>
2016-10-01 15:40:13 -0400

    added server/index.html

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 var port = 8000;
 let app = express();
 
 app.get('/*', (req, res) => {
-  res.send('Hello Ridgewood!')
+  // res.send('Hello Ridgewood!')
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(port, () => console.log('Running on localhost: ' + port));
