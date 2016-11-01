2b50743f7358f56f4cec7502ed58766ea1b4ace0
rodk <rod2010rod2010@hotmail.com>
2016-11-01 19:55:56 -0400

    #5 completed 'username' form prep

--- client/components/signup/signup-form.js
+++ client/components/signup/signup-form.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 class SignupForm extends React.Component {
+  constructor(props) { 
+    super(props);
+    this.state = {
+      username: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+  	e.preventDefault();
+  	console.log(this.state);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <h1>Join our community!</h1>
-
         <div className="form-group">
           <label className="control-label">Username</label>
           <input
+            value={this.state.username}
+            onChange={this.onChange}
             type="text"
             name="username"
             className="form-control"
